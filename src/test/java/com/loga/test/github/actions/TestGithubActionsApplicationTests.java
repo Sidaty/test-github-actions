2451351882debb29740662b5d7949610e761732b
Sidaty <ksidatym@gmail.com>
2020-03-14 15:05:40 +0000

    Initial commit

---
+++ src/test/java/com/loga/test/github/actions/TestGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.loga.test.github.actions;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class TestGithubActionsApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
